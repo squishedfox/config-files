946457139ff30c533a87d83d0e9dea97fc8e9eae
squishedfox <squishedfox@protonmail.com>
2026-05-23 12:38:27 -0500

    Remove uneeded root markers

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -93,14 +93,6 @@ cmp.setup.cmdline("/", {
 vim.lsp.config("rust_analyzer", {
   on_attach = on_attach,
   capabilities = capabilities,
-  -- cmd = { "rust-analyzer" },
-  root_markers = { "Cargo.toml", "rust-project.json" },
- -- settings = {
- --   ["rust-analyzer"] = {
- --     cargo = { allFeatures = true },
- --     checkOnSave =  true,
- --   },
- -- },
 })
 
 -- Lua
